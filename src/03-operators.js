b264d3799be1d1c5f0607689bb7e3e8edebc92c0
Joan Alsina Morillo <joan.alsina.morillo@upc.edu>
2026-05-15 15:41:23 +0200

    clase 6

---
+++ src/03-operators.js
@@ -0,0 +1,52 @@
+// Operadores aritméticos
+const suma = 5 + 3; // 8
+console.log(suma);
+
+const resta = 5 - 3; // 2
+console.log(resta);
+
+const multiplicacion = 5 * 3; // 15
+console.log(multiplicacion);
+
+const division = 6 / 3; // 2
+console.log(division);
+
+const modulo = 5 % 2; // 1
+console.log(modulo);
+
+const potencia = 2 ** 3; // 8
+console.log(potencia);
+
+
+// Operadores de asignación
+let a = 5;
+a += 3; // a = a + 3
+console.log(a); // 8
+
+let b = 10;
+b -= 4; // b = b - 4
+console.log(b); // 6
+
+let c = 6;
+c *= 2; // c = c * 2
+console.log(c); // 12
+
+let d = 15;
+d /= 3; // d = d / 3
+console.log(d); // 5
+
+
+//Operadores de incremento y decremento
+let contador = 0;
+contador++; // incrementa en 1
+console.log(contador); // 1
+
+contador--; // decrementa en 1
+console.log(contador); // 0
+
+
+
+console.log(1 / 0);   // Infinity
+console.log(-1 / 0);  // -Infinity
+console.log(0 / 0);   // NaN
+console.log('hola' * 2); // NaN
