8939b3a46fd3765664ae03ff0a7796592f2b8dcf
Joan <joan.alsina.morillo@upc.edu>
2026-05-15 15:00:34 +0200

    clases 1 y 2

---
+++ src/01-vars.js
@@ -0,0 +1,10 @@
+var bloquesRojos=true;
+var bloquesRojos=5;
+
+console.log(bloquesRojos);
+
+let contador = 0;
+let mensaje= "hola";
+
+
+console.log(contador);
---
+++ src/02-types.js
@@ -0,0 +1,6 @@
+const texto = "Hola";
+const numero = 42;
+const booleano = true;
+const indefinido = undefined;
+const simbolo = Symbol('id');
+const grande = 123n;
