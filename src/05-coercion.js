c0d3461bfa5e71c9b19de1b001b5471e9eccc0ce
Joan Alsina Morillo <joan.alsina.morillo@upc.edu>
2026-05-17 13:29:34 +0200

    Clase 9

---
+++ src/05-coercion.js
@@ -0,0 +1,16 @@
+// Coercion implícita
+
+console.log ('5' + 3);
+console.log ('5' - 3);
+console.log (true + 1);
+
+//Conversión Explicita
+
+const str = '42';
+const num = Number(str);
+console.log (typeof(str),typeof(num));
+const int = parseInt(str,10);
+console.log(int);
+const float = parseFloat('3.14159');
+console.log (float);
+
