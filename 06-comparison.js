7b18d0786c511fc5a2f854d4278da741076e05e7
Joan Alsina Morillo <joan.alsina.morillo@upc.edu>
2026-05-17 13:45:52 +0200

    clase 10

---
+++ 06-comparison.js
@@ -0,0 +1,25 @@
+//Operaciones de comparación en JS: Igualdad y desigualdad
+
+//Igualdad debil (==)
+
+console.log (5 == '5'); //true
+console.log (true == 1);  //true
+console.log (false == 0); //true
+console.log (null == undefined); //true
+
+//Desigualdad debil (!=)
+
+
+console.log (5 != '5'); 
+console.log (true != 1);  
+console.log (false != 0); 
+console.log (null != undefined);
+
+//Igualdad estricta (===)
+
+console.log (5 === '5'); 
+console.log (5 === '5');
+
+console.log (5 !== 5);
+console.log (5 !== '5');
+
