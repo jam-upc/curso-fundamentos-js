5e838fb9d3b2a2bc0c26d6e0c62cac864ac3efd5
Joan Alsina Morillo <joan.alsina.morillo@upc.edu>
2026-05-18 11:52:33 +0200

    clase 12

---
+++ src/08-if-else.js
@@ -0,0 +1,19 @@
+// IF  - ELSE
+
+const edad = 18;
+
+if (edad >= 18) {
+  console.log('eres mayor de edad');
+} else {
+  console.log('eres menor de edad');
+}
+
+const edad = 18;
+
+if (edad >= 19) {
+  console.log('eres mayor de edad');
+} else if (edad === 18) {
+  console.log('Tienes dieciocho años');
+} else {
+  console.log('eres menor de edad');
+}
