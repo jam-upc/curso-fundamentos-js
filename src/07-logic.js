50040b7a34555c039c77e2a3ca1361bba02ccbde
Joan Alsina Morillo <joan.alsina.morillo@upc.edu>
2026-05-18 11:38:57 +0200

    Clase 11

---
+++ src/07-logic.js
@@ -0,0 +1,17 @@
+// Operadores logicos
+
+
+// AND (&&)
+console.log(true && true);   // true
+console.log(true && false);  // false
+console.log(false && true);  // false
+
+// OR (||)
+console.log(true || true);   // true
+console.log(true || false);  // true
+console.log(false || false); // false
+
+
+// NOT (!)
+console.log(!true);   // false
+console.log(!false);  // true
